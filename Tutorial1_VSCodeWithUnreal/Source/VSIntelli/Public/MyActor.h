4183143e4a69e37a898a62031377f8050d8c15c3
Persp8ctiveGames <persp8ctive.games@gmail.com>
2025-11-28 23:54:42 -0500

    The source folder of turtorial 1

---
+++ Tutorial1_VSCodeWithUnreal/Source/VSIntelli/Public/MyActor.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class VSINTELLI_API AMyActor : public AActor {
+    GENERATED_BODY()
+
+  public:
+    // Sets default values for this actor's properties
+    AMyActor();
+
+  protected:
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+
+  public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+
+    // Called when actor is constructed (in editor or during gameplay)
+    virtual void OnConstruction(const FTransform& Transform) override;
+
+  protected:
+    // The cube mesh component
+    UPROPERTY(VisibleAnywhere)
+    UStaticMeshComponent *CubeMesh;
+
+    // Timer handle for debug printing
+    FTimerHandle DebugMessageTimerHandle;
+
+    // Function to print debug message
+    UFUNCTION()
+    void PrintDebugMessage();
+};
